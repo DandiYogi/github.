0b5a165d13e455fb41e8a68914f9ca7a82816a03
DandiYogi <126486220+DandiYogi@users.noreply.github.com>
2023-06-26 19:22:02 +0700

    Add files via upload

---
+++ 13/15.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="bg-primary">
+        <h2 class="text-light">Background color With Contrasting Text color</h2>
+    </div>
+<br>
+<img src="assets/img/logo.png" class="rounded-circle" alt="logo">
+<div class="container mt-3">
+    <h2>Basic Table</h2>
+    <p>the table class adds basic styling (light padding and horizontal dividers)to a table:</p>
+    <table class="table table-hover table-dark">
+        <thead>
+           <tr>
+            <td>Firstname</td>
+            <td>Lastname</t>
+            <td>Email</td>
+            <td>Alamat</td>
+           </tr> 
+        </thead>
+        <tbody>
+            <tr>
+                <td>Muhammad</td>
+                <td>Taufiq</td>
+                <td>john@example.com</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+                <td>Jepang</td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
